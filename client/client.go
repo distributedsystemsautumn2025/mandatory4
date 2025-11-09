11b6f003a1b2bb9b5e057ba12a4d274e702632f8
Milja <miljanoomi@gmail.com>
2025-11-09 18:16:14 +0100

    reconfigure client

--- client/client.go
+++ client/client.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"os"
@@ -19,14 +21,19 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 )
 
+type Queue struct {
+	Elements []*pb.Message
+	Size     int
+}
 type node struct {
-	pb.UnimplementedRicartAgrawalaServiceServer
-	id    string
-	clock int64
-	state string
-
-	//key: peer node ID, value: grpc client
-	peers map[string]pb.RicartAgrawalaServiceClient
+	server    pb.UnimplementedRicartAgrawalaServiceServer
+	client    pb.RicartAgrawalaServiceClient
+	id        string
+	clock     int64
+	state     string
+	replies   [3]*pb.Message
+	id2client map[string]*pb.RicartAgrawalaServiceClient
+	queue     Queue
 }
 
 var idToPort = map[string]string{
@@ -59,27 +66,27 @@ func main() {
 	log.Printf("Node %s listening on %s", *id, nodeAddress)
 
 	node := &node{
-		id:    *id,
-		clock: 0,
-		state: "released",
-		peers: make(map[string]pb.RicartAgrawalaServiceClient),
+		id:      *id,
+		clock:   0,
+		state:   "RELEASED",
+		replies: [3]*pb.Message{},
+		queue:   Queue{Size: 3},
 	}
 
 	grpcServer := grpc.NewServer()
 	pb.RegisterRicartAgrawalaServiceServer(grpcServer, node)
-
-	go func() {
-		if err := grpcServer.Serve(lis); err != nil {
-			log.Fatalf("failed to serve: %v", err)
-		}
-	}()
-
-	node.connectToPeers()
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+	log.Printf("Node %s is serving on %s", *id, nodeAddress)
 
 	//graceful shutdown
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	// do node stuff
+	go func() {
 
+	}()
 	// blocks code below, till signal is received
 	<-signalChan
 	log.Println("received termination signal, shutting down gracefully ...")
@@ -90,29 +97,140 @@ func main() {
 
 }
 
-/*func (node *node) multicast(msg *pb.AccessRequest) {
-	for peer := range n.peers {
-		node.peers[peer] <- msg
-	}
-}*/
-
 // here we attempt to connect to other nodes
-func (node *node) connectToPeers() {
-	//check that you don't attempt to connect to yourself:
+func (node *node) ConnectToPeers() {
+	node.id2client = make(map[string]*pb.RicartAgrawalaServiceClient)
+
 	for peerID, addr := range idToPort {
+		//check that you don't attempt to connect to yourself:
 		if peerID == node.id {
 			continue
 		}
-
 		conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 		if err != nil {
-			log.Printf("Failed to connect to peer %s at %s: %v", peerID, addr, err)
-			continue
+			log.Fatalf("failed to connect to peer: %s \n at port: %s", peerID, addr)
 		}
 
 		client := pb.NewRicartAgrawalaServiceClient(conn)
-		node.peers[peerID] = client
-
-		log.Printf("Node %s connected to peer %s at %s", node.id, peerID, addr)
+		node.id2client[peerID] = &client
+		log.Printf("Node %s connected to peer %s \n at %s", node.id, peerID, addr)
 	}
+}
+
+// RequestReply is the server-side implementation of our grpc service
+func (node *node) RequestReply(ctx context.Context, request *pb.Message) (*pb.Message, error) {
+	log.Printf("Node %s received an access request from node %s at time: %d", node.id, request.Sender, request.LogicalTime)
+	return node.RequestHandler(request), nil
+}
+
+// RequestHandler handles incoming requests by using the Ricart-Agrawala algorithm
+func (node *node) RequestHandler(request *pb.Message) *pb.Message {
+	if request.Sender == node.id {
+
+	}
+	if node.state == "HELD" || (node.state == "WANTED" && node.clock < request.LogicalTime) {
+		node.queue.Enqueue(request)
+	} else if node.state == "WANTED" && node.clock == request.LogicalTime {
+		if node.id < request.Sender {
+			node.queue.Enqueue(request)
+		} else {
+			return node.Reply()
+		}
+	}
+	return node.Reply()
+}
+
+// Requester sends an access request out to all other nodes
+func (node *node) Requester(request *pb.Message) {
+	// some clock stuff
+	node.state = "WANTED"
+	for clientID, client := range node.id2client {
+		go func(clientID string, client *pb.RicartAgrawalaServiceClient) {
+			ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+			defer cancel()
+			reply, err := RequestReply(ctx, request)
+			{
+				if err != nil {
+					log.Printf("Failed to request reply: %v", err)
+				}
+			}
+			log.Printf("Node %s is received a reply from client %s at time: %d", node.id, clientID, reply.LogicalTime)
+		}(clientID, client)
+	}
+	// check n-1 replies
+	if node.AccessGranted() {
+		node.CriticalSection()
+	}
+}
+
+// AccessGranted checks if there are sufficient replies for this node to enter the Critical Section
+func (node *node) AccessGranted() bool {
+	// we realise this is not a scalable solution
+	switch node.id {
+	case "1":
+		if node.replies[1] != nil && node.replies[2] != nil {
+			return true
+		}
+	case "2":
+		if node.replies[0] != nil && node.replies[2] == nil {
+			return true
+		}
+	case "3":
+		if node.replies[0] != nil && node.replies[1] == nil {
+			return true
+		}
+	}
+	return false
+}
+func (node *node) CriticalSection() {
+	node.state = "HELD"
+	log.Printf("%s is now in the Critical Section", node.id)
+	node.LeaveCriticalSection()
+}
+func (node *node) LeaveCriticalSection() {
+	node.state = "RELEASED"
+	log.Printf("%s has left the Critical Section", node.id)
+	for !node.queue.IsEmpty() {
+		//request := node.queue.Dequeue()
+		//node.Reply()
+	}
+}
+
+func (node *node) Reply() *pb.Message {
+	reply := pb.Message{
+		Sender:         node.id,
+		RequestOrReply: "REPLY",
+		LogicalTime:    node.clock,
+	}
+	return &reply
+}
+
+// queue stuff
+func (q *Queue) Enqueue(elem *pb.Message) {
+	if q.GetLength() == q.Size {
+		fmt.Println("Overflow")
+		return
+	}
+	q.Elements = append(q.Elements, elem)
+}
+
+func (q *Queue) Dequeue() *pb.Message {
+	if q.IsEmpty() {
+		fmt.Println("Queue is empty")
+		return nil
+	}
+	element := q.Elements[0]
+	if q.GetLength() == 1 {
+		q.Elements = nil
+		return element
+	}
+	q.Elements = q.Elements[1:]
+	return element // Slice off the element once it is dequeued.
+}
+func (q *Queue) GetLength() int {
+	return len(q.Elements)
+}
+
+func (q *Queue) IsEmpty() bool {
+	return len(q.Elements) == 0
 }
