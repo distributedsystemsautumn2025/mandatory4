d304b694488ac8a43972106d7d4a09733d9dae70
Milja <miljanoomi@gmail.com>
2025-11-09 21:32:36 +0100

    fix wait for replies
    didnt wait for replies before so didnt really work, now it waits for all replies and then accesses the critical section

--- client/client.go
+++ client/client.go
@@ -6,15 +6,13 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	pb "mandatory4/grpc"
 	"net"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 	"time"
-
-	// "context"
-	// "time"
-	pb "mandatory4/grpc"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -26,7 +24,6 @@ type Queue struct {
 }
 type node struct {
 	pb.UnimplementedRicartAgrawalaServiceServer
-	pb.RicartAgrawalaServiceClient
 	id        string
 	clock     int64
 	state     string
@@ -66,6 +63,12 @@ func main() {
 	if !ok {
 		log.Fatalf("Did not find address for id=%s", *id)
 	}
+	// listen
+	lis, err := net.Listen("tcp", nodeAddress)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	log.Printf("Node %s listening on %s", *id, nodeAddress)
 	node := &node{
 		id:        *id,
 		clock:     0,
@@ -74,37 +77,36 @@ func main() {
 		queue:     Queue{Size: 3},
 		id2client: map[string]pb.RicartAgrawalaServiceClient{},
 	}
-	// listen
-	lis, err := net.Listen("tcp", nodeAddress)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-	log.Printf("Node %s listening on %s", *id, nodeAddress)
+
 	// serve
 	grpcServer := grpc.NewServer()
 	pb.RegisterRicartAgrawalaServiceServer(grpcServer, node)
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
-	log.Printf("Node %s is serving on %s", *id, nodeAddress)
-
-	// do node stuff i.e.: listen for CLI-args
+	go func() {
+		if err := grpcServer.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+		log.Printf("Node %s is serving on %s", *id, nodeAddress)
+	}()
 
 	node.ConnectToPeers()
+
+	//graceful shutdown
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	// do node stuff i.e.: listen for CLI-args
 	go func() {
 		for {
 			_, err := bufio.NewReader(os.Stdin).ReadString('\n')
 			if err != nil {
 				log.Printf("Error reading CLI: %v", err)
 			}
-			node.Requester()
 			fmt.Println("sending request...")
+			node.Requester()
+			fmt.Println("requests sent")
 
 		}
 	}()
-	//graceful shutdown
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+
 	// blocks code below, till signal is received
 	<-signalChan
 	log.Println("received termination signal, shutting down gracefully ...")
@@ -116,11 +118,11 @@ func main() {
 }
 
 // here we attempt to connect to other nodes
-func (n *node) ConnectToPeers() {
-	n.id2client = make(map[string]pb.RicartAgrawalaServiceClient)
+func (node *node) ConnectToPeers() {
+	node.id2client = make(map[string]pb.RicartAgrawalaServiceClient)
 	for peerID, addr := range idToPort {
 		//check that you don't attempt to connect to yourself:
-		if peerID == n.id {
+		if peerID == node.id {
 			continue
 		}
 		conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
@@ -129,22 +131,22 @@ func (n *node) ConnectToPeers() {
 		}
 
 		client := pb.NewRicartAgrawalaServiceClient(conn)
-		n.id2client[peerID] = client
-		log.Printf("Node %s connected to peer %s \n at %s", n.id, peerID, addr)
+		node.id2client[peerID] = client
+		log.Printf("Node %s connected to peer %s at %s", node.id, peerID, addr)
+		node.clock++
 	}
 }
 
-// RequestReply is the server-side implementation of our grpc service
+// RequestAndReply is the server-side implementation of our grpc service
 func (node *node) RequestAndReply(ctx context.Context, request *pb.Message) (*pb.Message, error) {
-	log.Printf("Node %s received an access request from node %s at time: %d", node.id, request.Sender, request.LogicalTime)
+	node.clock++
+	log.Printf("Node %s received an access request from node %s at time: %d", node.id, request.Sender, node.clock)
 	return node.RequestHandler(request), nil
 }
 
 // RequestHandler handles incoming requests by using the Ricart-Agrawala algorithm
 func (node *node) RequestHandler(request *pb.Message) *pb.Message {
-	if request.Sender == node.id {
-
-	}
+	node.clock++
 	if node.state == "HELD" || (node.state == "WANTED" && node.clock < request.LogicalTime) {
 		node.queue.Enqueue(request)
 	} else if node.state == "WANTED" && node.clock == request.LogicalTime {
@@ -159,26 +161,34 @@ func (node *node) RequestHandler(request *pb.Message) *pb.Message {
 
 // Requester sends an access request out to all other nodes
 func (node *node) Requester() {
+	node.clock++
 	request := &pb.Message{
 		Sender:         node.id,
 		RequestOrReply: "REQUEST",
 		LogicalTime:    node.clock,
 	}
-	// some clock stuff
 	node.state = "WANTED"
+	var wg sync.WaitGroup
 	for clientID, client := range node.id2client {
+		wg.Add(1)
 		go func(clientID string, client pb.RicartAgrawalaServiceClient) {
+			defer wg.Done()
+			node.clock++
 			ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 			defer cancel()
 			reply, err := client.RequestAndReply(ctx, request)
 			if err != nil {
 				log.Printf("Failed to request reply: %v", err)
 			}
-			log.Printf("Node %s is received a reply from client %s at time: %d", node.id, clientID, reply.LogicalTime)
+			log.Printf("Node %s has received a reply from client %s at time: %d", node.id, clientID, node.clock)
+			index := id2index(clientID)
+			node.replies[index] = reply
 		}(clientID, client)
 	}
+	wg.Wait()
 	// check n-1 replies
 	if node.AccessGranted() {
+		fmt.Println("Access granted")
 		node.CriticalSection()
 	}
 }
@@ -189,30 +199,50 @@ func (node *node) AccessGranted() bool {
 	switch node.id {
 	case "1":
 		if node.replies[1] != nil && node.replies[2] != nil {
+			node.ResetReplies(1, 2)
 			return true
 		}
 	case "2":
-		if node.replies[0] != nil && node.replies[2] == nil {
+		if node.replies[0] != nil && node.replies[2] != nil {
 			return true
 		}
 	case "3":
-		if node.replies[0] != nil && node.replies[1] == nil {
+		if node.replies[0] != nil && node.replies[1] != nil {
 			return true
 		}
 	}
 	return false
 }
+
+func (node *node) ResetReplies(index1 int, index2 int) {
+	node.replies[index1] = nil
+	node.replies[index2] = nil
+}
 func (node *node) CriticalSection() {
+	node.clock++
 	node.state = "HELD"
-	log.Printf("%s is now in the Critical Section", node.id)
+	time.Sleep(3 * time.Second)
+	log.Printf("Node %s is now in the Critical Section at logical time: %d", node.id, node.clock)
 	node.LeaveCriticalSection()
 }
 func (node *node) LeaveCriticalSection() {
+	node.clock++
 	node.state = "RELEASED"
-	log.Printf("%s has left the Critical Section", node.id)
+	log.Printf("Node %s has left the Critical Section at logical time: %d", node.id, node.clock)
 	for !node.queue.IsEmpty() {
-		//request := node.queue.Dequeue()
-		//node.Reply()
+		node.clock++
+		request := node.queue.Dequeue()
+		clientID := request.Sender
+		client := node.id2client[clientID]
+		go func(clientID string, client pb.RicartAgrawalaServiceClient) {
+			ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+			defer cancel()
+			reply, err := node.RequestAndReply(ctx, request)
+			if err != nil {
+				log.Printf("Failed to request reply: %v", err)
+			}
+			log.Printf("Node %s is replying to client %s at time: %d", node.id, clientID, reply.LogicalTime)
+		}(clientID, client)
 	}
 }
 
@@ -223,6 +253,18 @@ func (node *node) Reply() *pb.Message {
 		LogicalTime:    node.clock,
 	}
 	return &reply
+}
+
+func id2index(id string) int {
+	switch id {
+	case "1":
+		return 0
+	case "2":
+		return 1
+	case "3":
+		return 2
+	}
+	return 0
 }
 
 // queue stuff
